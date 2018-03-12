4c407440b96222872cde553bc17f790c4f1ce1ee
ShawnXxy <shawnxxy@hotmail.com>
2018-03-11 19:35:11 -0500

    fix links

--- includes/navigation.php
+++ includes/navigation.php
@@ -23,8 +23,9 @@
                         $query = mysqli_query($con, $sql);
 
                         while($row = mysqli_fetch_assoc($query)) {
+                            $cat_id = $row['cat_id'];
                             $cat_title = $row['cat_title'];
-                            echo "<li><a href='#'>{$cat_title}</a></li>";
+                            echo "<li><a href='category.php?category={$cat_id}'>{$cat_title}</a></li>";
                         }
                     ?>
                     <li><a href="admin">Admin</a></li>
