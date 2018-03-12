c7b6c9730944b2f855c8adffe33a6262dccf1add
ShawnXxy <shawnxxy@hotmail.com>
2018-03-11 22:42:13 -0500

    get counts

--- admin/index.php
+++ admin/index.php
@@ -29,6 +29,117 @@
             </div>
             <!-- /.row -->
 
+            <div class="row">
+                <div class="col-lg-3 col-md-6">
+                    <div class="panel panel-primary">
+                        <div class="panel-heading">
+                            <div class="row">
+                                <div class="col-xs-3">
+                                    <i class="fa fa-file-text fa-5x"></i>
+                                </div>
+                                <div class="col-xs-9 text-right">
+                                    <?php
+                                        $sql = "SELECT * FROM {$table_posts};";
+                                        $query = mysqli_query($con, $sql);
+                                        $post_counts = mysqli_num_rows($query);
+                                        echo "<div class='huge'>{$post_counts}</div>";
+                                    ?>
+                                    <div>Posts</div>
+                                </div>
+                            </div>
+                        </div>
+                        <a href="posts.php">
+                            <div class="panel-footer">
+                                <span class="pull-left">View Details</span>
+                                <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+                                <div class="clearfix"></div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6">
+                    <div class="panel panel-green">
+                        <div class="panel-heading">
+                            <div class="row">
+                                <div class="col-xs-3">
+                                    <i class="fa fa-comments fa-5x"></i>
+                                </div>
+                                <div class="col-xs-9 text-right">
+                                    <?php
+                                        $sql = "SELECT * FROM {$table_comments};";
+                                        $query = mysqli_query($con, $sql);
+                                        $comments_counts = mysqli_num_rows($query);
+                                        echo "<div class='huge'>{$comments_counts}</div>";
+                                    ?>
+                                    <div>Comments</div>
+                                </div>
+                            </div>
+                        </div>
+                        <a href="comments.php">
+                            <div class="panel-footer">
+                                <span class="pull-left">View Details</span>
+                                <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+                                <div class="clearfix"></div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6">
+                    <div class="panel panel-yellow">
+                        <div class="panel-heading">
+                            <div class="row">
+                                <div class="col-xs-3">
+                                    <i class="fa fa-user fa-5x"></i>
+                                </div>
+                                <div class="col-xs-9 text-right">
+                                    <?php
+                                        $sql = "SELECT * FROM {$table_users};";
+                                        $query = mysqli_query($con, $sql);
+                                        $users_counts = mysqli_num_rows($query);
+                                        echo "<div class='huge'>{$users_counts}</div>";
+                                    ?>
+                                    <div> Users</div>
+                                </div>
+                            </div>
+                        </div>
+                        <a href="users.php">
+                            <div class="panel-footer">
+                                <span class="pull-left">View Details</span>
+                                <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+                                <div class="clearfix"></div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+                <div class="col-lg-3 col-md-6">
+                    <div class="panel panel-red">
+                        <div class="panel-heading">
+                            <div class="row">
+                                <div class="col-xs-3">
+                                    <i class="fa fa-list fa-5x"></i>
+                                </div>
+                                <div class="col-xs-9 text-right">
+                                    <?php
+                                        $sql = "SELECT * FROM {$table_cat};";
+                                        $query = mysqli_query($con, $sql);
+                                        $cat_counts = mysqli_num_rows($query);
+                                        echo "<div class='huge'>{$cat_counts}</div>";
+                                    ?>
+                                    <div>Categories</div>
+                                </div>
+                            </div>
+                        </div>
+                        <a href="categories.php">
+                            <div class="panel-footer">
+                                <span class="pull-left">View Details</span>
+                                <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+                                <div class="clearfix"></div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+            </div>
+
         </div>
         <!-- /.container-fluid -->
 
